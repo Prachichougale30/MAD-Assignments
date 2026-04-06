53d91a68dfd91b8083e9d84619c5f18af8a70439
Prachichougale30 <prachichougale2530@gmail.com>
2026-04-06 20:44:11 +0530

    Implement MainActivity to load FirstFragment

---
+++ Assignment 2/Activites And Fragments/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.activitesandfragments;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FirstFragment fragment = new FirstFragment();
+
+        FragmentTransaction transaction =
+                getSupportFragmentManager().beginTransaction();
+
+        transaction.replace(R.id.fragment_container, fragment);
+        transaction.commit();
+    }
+}
